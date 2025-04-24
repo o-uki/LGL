f56840e0cfaec5f31590919bcc5795762a3b85a0
o-uki <makaroniaji024@gmail.com>
2025-04-24 23:39:32 +0900

    トークンを設定。

--- lgl.java
+++ lgl.java
@@ -2,7 +2,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import javax.swing.*;
+
+class LglToken {
+    String token;
+    String code;
+
+    public LglToken(String setToken, String setCode) {
+        this.token = setToken;
+        this.code = setCode;
+    }
+}
 
 public class Lgl {
     public static void main(String[] args) {
@@ -14,7 +23,19 @@ public class Lgl {
                 lglSourceCode += row;
             }
 
-            System.out.println(lglSourceCode);
+            // LGLのトークンの配列
+            LglToken[] lglTokens = new LglToken[4];
+            lglTokens[0] = new LglToken(":", "set");
+            lglTokens[1] = new LglToken("{", "nestBracketLeft");
+            lglTokens[2] = new LglToken("}", "nestBracketRight");
+            lglTokens[3] = new LglToken(".", "end");
+
+            // 一文字ずつ解析
+            for (int i = 0; i < lglSourceCode.length(); i++) {
+                for (Object token : lglTokens) {
+                    
+                }
+            }
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
